44c14bb324e051fbd10c8e2bf622c20246b0f449
Anton Voylenko <anton.voylenko@gmail.com>
2026-01-11 15:02:26 +0200

    feat: allow cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 require('./routes')
 const express = require('express')
+const cors = require('cors')
 const { routes } = require('./routes')
 const { maxAttachmentSize, basePath, trustProxy } = require('./config')
 
@@ -15,6 +16,11 @@ if (trustProxy) {
 
 app.use(express.json({ limit: maxAttachmentSize + 1000000 }))
 app.use(express.urlencoded({ limit: maxAttachmentSize + 1000000, extended: true }))
+
+app.use(cors({
+  origin: process.env.ALLOWED_ORIGINS?.split(',') || '*',
+  credentials: true
+}))
 
 // Mount routes with configurable base path
 app.use(basePath, routes)
